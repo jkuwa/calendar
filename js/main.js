a60e747ec54070bd9357afe1dfe442c08b526856
kuwa <163106848+jkuwa@users.noreply.github.com>
2024-12-27 12:17:18 +0900

    追加：JS処理

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+{
+  const calendar = document.querySelector(".js-calendar");
+  for ( let i=1; i<=31; i++ ) {
+    const date = calendar.innerHTML;
+
+    if( i % 7 === 0 ) {
+      const newDate = date + '<span>' + i +'</span><br>';
+      calendar.innerHTML = newDate;
+    } else if ( i === 1 || i % 7 === 1 ) {
+      const newDate = date + '<span class="c-red">' + i +'</span>';
+      calendar.innerHTML = newDate;
+    } else {
+      const newDate = date + '<span>' + i +'</span>';
+      calendar.innerHTML = newDate;
+    }
+  };
+}
